ce9a6b9e91db005b9e67eda797c432a93512b711
ricardo <riu.global@gmail.com>
2021-12-02 13:23:05 -0500

    Removed prerenderin of pages

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -1,3 +1,4 @@
+import { useState} from 'react';
 import ReactMarkdown from "react-markdown";
 import Moment from "react-moment";
 import { fetchAPI } from "../../lib/api";
@@ -5,14 +6,30 @@ import Layout from "../../components/layout";
 import Image from "../../components/image";
 import Seo from "../../components/seo";
 import { getStrapiMedia } from "../../lib/media";
+import { useEffect } from "react";
 
-const Article = ({ article, categories }) => {
-  const imageUrl = getStrapiMedia(article.image);
+const Article = ({slug}) => {
+  const [article, setArticle] = useState()
+  const [categories, setCategories] = useState([])
+  useEffect(async () => {
+    const articles =  await fetchAPI(
+      `/articles?slug=${slug}`
+    );
+    const categories =  await fetchAPI("/categories");
+    
+    setArticle(articles[0])
+    setCategories(categories)
+    // return {
+    //   props: { article: articles[0], categories },
+    //   // revalidate: 1,
+    // };
+  }, [])
+  const imageUrl = getStrapiMedia(article?.image);
 
   const seo = {
-    metaTitle: article.title,
-    metaDescription: article.description,
-    shareImage: article.image,
+    metaTitle: article?.title,
+    metaDescription: article?.description,
+    shareImage: article?.image,
     article: true,
   };
 
@@ -26,17 +43,17 @@ const Article = ({ article, categories }) => {
         data-srcset={imageUrl}
         data-uk-img
       >
-        <h1>{article.title}</h1>
+        <h1>{article?.title}</h1>
       </div>
       <div className="uk-section">
         <div className="uk-container uk-container-small">
-          <ReactMarkdown source={article.content} escapeHtml={false} />
+          <ReactMarkdown source={article?.content} escapeHtml={false} />
           <hr className="uk-divider-small" />
           <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
             <div>
-              {article.author.picture && (
+              {article?.author?.picture && (
                 <Image
-                  image={article.author.picture}
+                  image={article?.author.picture}
                   style={{
                     position: "static",
                     borderRadius: "50%",
@@ -47,10 +64,10 @@ const Article = ({ article, categories }) => {
             </div>
             <div className="uk-width-expand">
               <p className="uk-margin-remove-bottom">
-                By {article.author.name}
+                By {article?.author?.name}
               </p>
               <p className="uk-text-meta uk-margin-remove-top">
-                <Moment format="MMM Do YYYY">{article.published_at}</Moment>
+                <Moment format="MMM Do YYYY">{article?.published_at}</Moment>
               </p>
             </div>
           </div>
@@ -60,29 +77,36 @@ const Article = ({ article, categories }) => {
   );
 };
 
-export async function getStaticPaths() {
-  const articles = await fetchAPI("/articles");
+// export async function getStaticPaths() {
+//   const articles = await fetchAPI("/articles");
 
+//   return {
+//     paths: articles.map((article) => ({
+//       params: {
+//         slug: article.slug,
+//       },
+//     })),
+//     fallback: false,
+//   };
+// }
+
+export async function getServerSideProps({ params }) {
   return {
-    paths: articles.map((article) => ({
-      params: {
-        slug: article.slug,
-      },
-    })),
-    fallback: false,
+    props: { slug: params.slug },
+    // revalidate: 1,
   };
 }
 
-export async function getStaticProps({ params }) {
-  const articles = await fetchAPI(
-    `/articles?slug=${params.slug}`
-  );
-  const categories = await fetchAPI("/categories");
+// export async function getServerSideProps({ params }) {
+//   const articles = await fetchAPI(
+//     `/articles?slug=${params.slug}`
+//   );
+//   const categories = await fetchAPI("/categories");
 
-  return {
-    props: { article: articles[0], categories },
-    revalidate: 1,
-  };
-}
+//   return {
+//     props: { article: articles[0], categories },
+//     // revalidate: 1,
+//   };
+// }
 
 export default Article;
